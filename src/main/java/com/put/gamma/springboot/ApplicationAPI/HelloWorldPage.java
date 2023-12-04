dd81e76ff19f7b0573362c26458534fe62265a59
Andrei Kartavik <andreykartavik@gmail.com>
2023-12-04 20:58:04 +0100

    Test file upload via RestAPI

--- src/main/java/com/put/gamma/springboot/ApplicationAPI/HelloWorldPage.java
+++ src/main/java/com/put/gamma/springboot/ApplicationAPI/HelloWorldPage.java
@@ -1,8 +1,11 @@
 package com.put.gamma.springboot.ApplicationAPI;
 
 import com.put.gamma.springboot.TestFunctionality.Document;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
 
 @RestController
 public class HelloWorldPage {
@@ -12,4 +15,27 @@ public class HelloWorldPage {
         return "Book: " + document.getTitle() + "\nAuthor: " + document.getAuthor();
     }
 
+    @PostMapping("/submit")
+    public String addDocument(@RequestParam("file") MultipartFile file) {
+        if (file.isEmpty()) {
+            return "File is empty.";
+        }
+
+        try {
+            // Get the bytes of the file content
+            byte[] bytes = file.getBytes();
+
+            // Convert bytes to String assuming it's a text file (adjust charset if needed)
+            String fileContent = new String(bytes);
+
+            // Print or process the file content
+            System.out.println("Received file: " + file.getOriginalFilename());
+            System.out.println("File content: " + fileContent);
+
+            return file.getOriginalFilename();
+        } catch (IOException e) {
+            // Handle exception (e.g., log it)
+            return "Failed to process the file.";
+        }
+    }
 }
