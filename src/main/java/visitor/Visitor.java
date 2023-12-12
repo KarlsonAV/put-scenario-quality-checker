65abfb3e52350183a5b8158e7572e559d3fe1f0e
deptage <zuzialawn@gmail.com>
2023-12-12 14:53:16 +0100

    Added non-functional visitor

---
+++ src/main/java/visitor/Visitor.java
@@ -0,0 +1,13 @@
+package visitor;
+
+import scenario.Scenario;
+import section.Section;
+
+public class Visitor {
+    public void visit(Scenario scenario){
+        return;
+    }
+    public void visit(Section section){
+        return;
+    }
+}
