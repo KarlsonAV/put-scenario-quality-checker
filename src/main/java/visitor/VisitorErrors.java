03dff3670e9d96648bd425cbadf3ecbdcb57818e
Jakub Brambor <111154360+august3sas@users.noreply.github.com>
2023-12-14 15:29:52 +0100

    updated checking the keywords

--- src/main/java/visitor/VisitorErrors.java
+++ src/main/java/visitor/VisitorErrors.java
@@ -9,15 +9,17 @@ import java.util.List;
 public class VisitorErrors implements Visitor{
     List<String> errors= new ArrayList<>();
     List<String> allActors = new ArrayList<>();
+    List<String> keywords = new ArrayList<String>();
     @Override
     public void visit(Scenario scenario){
         allActors.addAll(scenario.getSystemActors());
         allActors.addAll(scenario.getActors());
+        keywords = scenario.keywords;
         scenario.accept(this);
     }
     @Override
     public void visit(Section section){
-        if(!section.checkIfBeginsWithActorName(allActors)){
+        if(!section.checkIfBeginsWithActorName(allActors)&&!section.checkIfBeginsWithKeyword(keywords)){
             errors.add(section.content);
         }
         section.accept(this);
