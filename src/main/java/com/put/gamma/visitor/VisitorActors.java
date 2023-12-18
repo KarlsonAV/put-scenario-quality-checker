f62e6699ca7a69ca0137de5b59237a711f3d2bd6
Jakub Brambor <111154360+august3sas@users.noreply.github.com>
2023-12-18 18:53:56 +0100

    created documentation

--- src/main/java/com/put/gamma/visitor/VisitorActors.java
+++ src/main/java/com/put/gamma/visitor/VisitorActors.java
@@ -5,17 +5,29 @@ import com.put.gamma.section.Section;
 
 import java.util.ArrayList;
 import java.util.List;
-
+/**
+ * This class implements Visitor interface and is suited for checking
+ * the actors and system actors, and checking if sections begin with
+ * an actor name.
+ */
 public class VisitorActors implements Visitor {
 
     List<String> allActors = new ArrayList<>();
     boolean doesStartWithActors=false;
+    /**
+     * This method visits the scenario and is only let into just the scenario object.
+     * @param scenario - object of Scenario type
+     */
     @Override
     public void visit(Scenario scenario) {
         allActors.addAll(scenario.getSystemActors());
         allActors.addAll(scenario.getActors());
         scenario.acceptOnlyHere(this);
     }
+    /**
+     * This method visits the section and is only let into one section at a time.
+     * @param section - object of Section type
+     */
     @Override
     public void visit(Section section) {
         section.acceptOnlyHere(this);
